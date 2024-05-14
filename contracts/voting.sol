744c1a6f9466d138d5532521218f00b4e05e05dd
Ahmad Abdur Rahman Khan <163046609+ahmadkhan100@users.noreply.github.com>
2024-05-15 02:28:33 +0530

    Update voting.sol

--- contracts/voting.sol
+++ contracts/voting.sol
@@ -1,1 +1,62 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+contract Voting {
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+
+    struct Voter {
+        bool authorized;
+        bool voted;
+        uint vote;
+    }
+
+    address public owner;
+    string public electionName;
+
+    mapping(address => Voter) public voters;
+    Candidate[] public candidates;
+    uint public totalVotes;
+
+    modifier ownerOnly() {
+        require(msg.sender == owner, "Only owner can call this function");
+        _;
+    }
+
+    constructor(string memory _name) {
+        owner = msg.sender;
+        electionName = _name;
+    }
+
+    function addCandidate(string memory _name) ownerOnly public {
+        candidates.push(Candidate(candidates.length, _name, 0));
+    }
+
+    function authorize(address _person) ownerOnly public {
+        voters[_person].authorized = true;
+    }
+
+    function vote(uint _voteIndex) public {
+        require(!voters[msg.sender].voted, "You have already voted");
+        require(voters[msg.sender].authorized, "You are not authorized to vote");
+
+        voters[msg.sender].vote = _voteIndex;
+        voters[msg.sender].voted = true;
+
+        candidates[_voteIndex].voteCount += 1;
+        totalVotes += 1;
+    }
+
+    function end() ownerOnly public view returns (Candidate memory) {
+        Candidate memory winner = candidates[0];
+        for (uint i = 1; i < candidates.length; i++) {
+            if (candidates[i].voteCount > winner.voteCount) {
+                winner = candidates[i];
+            }
+        }
+        return winner;
+    }
+}
